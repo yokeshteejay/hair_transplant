a5187ed0555ca9af35690b734cfb64b90626beb9
yokeshteejay <yokeshkatraj12102002@gmail.com>
2024-05-01 14:38:05 +0530

    hairtransplant

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <FormPage1/>
+    <FormPage1/> 
   </React.StrictMode>
 );
 
